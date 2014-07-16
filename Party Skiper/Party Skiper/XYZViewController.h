411539bdc2f27996e8b6bff076a2921ff4868dd7
cojoigo <cojoigo@gmail.com>
2014-07-16 16:25:50 -0400

    Unwind segue working

---
+++ Party Skiper/Party Skiper/XYZViewController.h
@@ -0,0 +1,13 @@
+//
+//  XYZViewController.h
+//  Party Skiper
+//
+//  Created by Connor Igo on 7/16/14.
+//  Copyright (c) 2014 Software Engineering 1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZViewController : UIViewController
+- (IBAction)unwindMainView:(UIStoryboardSegue *)segue;
+@end
---
+++ Party Skiper/Party Skiper/XYZViewController.m
@@ -0,0 +1,54 @@
+//
+//  XYZViewController.m
+//  Party Skiper
+//
+//  Created by Connor Igo on 7/16/14.
+//  Copyright (c) 2014 Software Engineering 1. All rights reserved.
+//
+
+#import "XYZViewController.h"
+
+@interface XYZViewController ()
+
+@end
+
+@implementation XYZViewController
+
+- (IBAction)unwindMainView:(UIStoryboardSegue *)segue
+{
+    
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
